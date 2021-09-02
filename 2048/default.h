d217cdbf387ad2fcf1d2abe7b9543a32ed6cd8b0
JayKim0202 <jayjkim0223@gmail.com>
2021-09-02 17:34:05 +0900

    del

--- 2048/default.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DEFAULT_H__
-#define __DEFAULT_H__
-
-#include "_3X3.h"
-
-#endif
--- rudder control/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
-#include "rudder_control.h"
-#include "line.h"
-
-#endif // !__RUDDER_CONTROL_H__
